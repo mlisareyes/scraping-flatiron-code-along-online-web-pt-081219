c89ccd2372d4c3521f2501546904065d1ca74371
Monica R <mlisareyes@gmail.com>
2019-09-27 03:13:07 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,12 +1,13 @@
 require 'nokogiri'
 require 'open-uri'
 require 'pry'
-
+ 
 require_relative './course.rb'
-
+ 
 class Scraper
-  
+ 
   def get_page
+ 
     Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
  
     #doc.css(".post").each do |post|
@@ -15,22 +16,5 @@ class Scraper
       #course.schedule = post.css(".date").text
       #course.description = post.css("p").text
     #end
-    end
   end
-  
-  def print_courses
-    self.make_courses
-    Course.all.each do |course|
-      if course.title && course.title != ""
-        puts "Title: #{course.title}"
-        puts "  Schedule: #{course.schedule}"
-        puts "  Description: #{course.description}"
-      end
-    end
-  end
-  
-end
-
-Scraper.new.get_page
-
-
+ 
